0cee8a14aaeb21ad7657766cfd4d6751b9bde896
mo21610 <garcia.morgane.21610@gmail.com>
2019-02-20 21:30:15 +0100

    Création BDD et espace édition de post pour l'administrateur

--- admin/editAdmin.php
+++ admin/editAdmin.php
@@ -1,3 +1,33 @@
+<?php
+    try // Connexion à la BDD
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+
+    if (!empty($_POST)) { // Si $_POST n'est pas vide 
+        $validation = true;
+        
+        if (empty($_POST)) {
+            $validation = false;
+        }
+
+        if ($validation == true) {
+            $req = $bdd->prepare('INSERT INTO admin (title, post, date_post) VALUES(:title, :post, NOW())'); // Requête sans la partie variable
+            $req->execute(array(  // Recuperation des variables de $_POST (issue du form) & insertion dans BDD
+                'title' => $_POST['title'],
+                'post' => $_POST['post'],
+            ));
+        }
+    }
+    // Récupération des post par ordre décroissant
+    $response = $bdd->query('SELECT title, post, date_post FROM admin ORDER BY id DESC');
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,6 +41,14 @@
 <body>
 
     <?php include("header_admin.php"); ?> 
+
+    <h2>Edition du texte:</h2>
+
+    <form action="editAdmin.php" method="post">
+        Titre: <br><input type="text" class="form_edit" name="title"><br>
+        Texte: <br><textarea name="post" class="form_edit" cols="100" rows="10"></textarea><br>
+        <button type="submit">Publier</button>
+    </form>
 
 </body>
 </html>
